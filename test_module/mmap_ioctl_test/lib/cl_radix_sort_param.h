12279e15448a086bdcdd8036c6b002e1af672e2d
transcender <transcender@transcender-Ubuntu>
2013-08-26 08:21:26 +0900

    mmap_ioctl_test_v0_1

---
+++ test_module/mmap_ioctl_test/lib/cl_radix_sort_param.h
@@ -0,0 +1,31 @@
+// C++ class for sorting integer list in OpenCL
+// copyright Philippe Helluy, Université de Strasbourg, France, 2011, helluy@math.unistra.fr
+// licensed under the GNU Lesser General Public License see http://www.gnu.org/copyleft/lesser.html
+// if you find this software usefull you can cite the following work in your reports or articles:
+// Philippe HELLUY, A portable implementation of the radix sort algorithm in OpenCL, HAL 2011.
+// global parameters for the CLRadixSort class
+// they are included in the class AND in the OpenCL kernels
+///////////////////////////////////////////////////////
+// these parameters can be changed
+#define _ITEMS  16 // number of items in a group
+#define _GROUPS 16 // the number of virtual processors is _ITEMS * _GROUPS
+#define  _HISTOSPLIT 512 // number of splits of the histogram
+#define _TOTALBITS 25  // number of bits for the integer in the list (max=32)
+#define _BITS 5  // number of bits in the radix
+// max size of the sorted vector
+// it has to be divisible by  _ITEMS * _GROUPS
+// (for other sizes, pad the list with big values)
+//#define _N (_ITEMS * _GROUPS * 16)
+#define _N (1<<20)  // maximal size of the list
+#define VERBOSE 0
+#define TRANSPOSE 0  // transpose the initial vector (faster memory access)
+#define PERMUT 1  // store the final permutation
+////////////////////////////////////////////////////////
+
+
+// the following parameters are computed from the previous
+#define _RADIX (1 << _BITS) //  radix  = 2^_BITS
+#define _PASS (_TOTALBITS/_BITS) // number of needed passes to sort the list
+#define _HISTOSIZE (_ITEMS * _GROUPS * _RADIX ) // size of the histogram
+// maximal value of integers for the sort to be correct
+#define _MAXINT (1 << (_TOTALBITS-1))
---
+++ test_module/mmap_ioctl_test/mmap_ioctl_module.c
@@ -0,0 +1,97 @@
+/* This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the GPL-COPYING file in the top-level directory.
+ *
+ * Copyright (c) 2010-2011 University of Utah and the Flux Group.
+ * All rights reserved.
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/spinlock.h>
+#include <linux/gfp.h>
+#include <linux/kthread.h>
+#include <linux/proc_fs.h>
+#include <linux/mm.h>
+#include <linux/mm_types.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
+#include <linux/random.h>
+#include <asm/page.h>
+#include <linux/timex.h>
+
+#include "../../qhgpu/qhgpu.h"
+
+
+static int mmap_ioctl_callback(struct qhgpu_request *req) {
+	printk("[mmap ioctl module]TIME: %10lu MS\n", tt);
+	struct completion *c = (struct completion*)req->kdata;
+	complete(c);
+
+	return 0;
+}
+
+
+static int __init minit(void) {
+	const unsigned int DATA_SIZE = 0x10000000;
+
+	struct qhgpu_request *req;
+
+	int* int_buf;
+	int* mmap_addr;
+
+	struct timeval t0, t1;
+	long tt;
+
+	printk("[mmap ioctl module]init\n");
+
+	//// alloc request
+	req = qhgpu_alloc_request();
+	if (!req) {
+		printk("request null\n");
+		return 0;
+	}
+
+	strcpy(req->service_name, "mmap_ioctl_service");
+	req->callback = mmap_ioctl_callback;
+	// init mmap_addr
+	req->kmmap_addr = qhgpu_mmap_addr_pass();
+	req->mmap_size = DATA_SIZE;
+
+	//set mmap_addr
+	mmap_addr = kmmap_addr;
+
+	//4096 * 2^10 == sizeof(int) * 2^10 * 2^10
+	int_buf = (unsigned int *)__get_free_pages(GFP_KERNEL, 10);
+
+	if(int_buf == NULL) {
+		printk("[mmap ioctl module]int_buf __get_free_pages error.\n", tt);
+		return;
+	}
+
+	do_gettimeofday(&t0);
+	memcpy();
+	do_gettimeofday(&t1);
+
+	tt = 1000000*(t1.tv_sec-t0.tv_sec) +
+			((long)(t1.tv_usec) - (long)(t0.tv_usec));
+
+	printk("[mmap ioctl module]TIME: %10lu MS\n", tt);
+
+	qhgpu_call_sync(req);
+
+	printk("[mmap ioctl module]init\n");
+
+	return 0;
+}
+
+static void __exit mexit(void) {
+	printk("[mmap ioctl module]exit\n");
+}
+
+module_init( minit);
+module_exit( mexit);
+
+MODULE_LICENSE("GPL");
